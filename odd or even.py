8dcc7f78d537a5b92168b802351ee26acb7463f2
KALAIYARASICHANDRASEKAR <39008832+KALAIYARASICHANDRASEKAR@users.noreply.github.com>
2019-08-13 08:33:31 -0700

    Update odd or even.py

--- odd or even.py
+++ odd or even.py
@@ -1,1 +1,8 @@
-
+#Find the number odd or even
+a=float(input("Enter the number:"))
+if a%2==0:
+    print("It's even number")
+elif a%2!=0:
+    print("It's odd number")
+else:
+    print("The number is invalid")
